032b8272cd760a6e95724ed9727ad03dfd0bdcd2
Luis Fernando Perez Rodriguez <luishost131@gmail.com>
2022-05-06 00:57:17 -0600

    cambio 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState,useEffect} from 'react';
+import { cleanup } from '@testing-library/react';
 
 function App() {
+  const url= 'https://jsonplaceholder.typicode.com/todos'
+  const [todos, setTodos]=useState()
+  const fetchApi = async () =>{
+    const response = await fetch(url)
+    const responseJSON = await response.json()
+    setTodos(responseJSON)
+  }
+  useEffect(() => {
+    fetchApi()
+  },[])
+    
   return (
     <div className="App">
       Hola mundo
+      <ul>
+      { !todos ? 'Cargando...':
+      todos.map( (todo,index)=>{
+        return<li>{todo.title }</li>
+      })
+      }
+      </ul>
     </div>
   );
 }
